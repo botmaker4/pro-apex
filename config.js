823c1e15eee2bdb36fc99001c73a29e61b99bc40
Glitch (maze-denim-durian) <none>
2021-01-26 14:31:47 +0000

    👩‍❤️‍💋‍👩💳 Updated with Glitch

---
+++ config.js
@@ -0,0 +1,7 @@
+exports.Token = ``; //Bot Token - Important
+exports.Default_Prefix = `+`; //Bot Default Prefix - Important
+exports.Color = `RANDOM`; //Bot All Embeds Color - Use CAPS For Name - Important
+exports.WelcomeImage = `https://cdn.discordapp.com/attachments/787989410959982592/794302125634355250/images_5.jpeg`; //Welcome Image Link - Default Image By Me (If No Link Provided)
+exports.LeaveImage = `https://cdn.discordapp.com/attachments/787989410959982592/794302125634355250/images_5.jpeg`; //Leave Image Link - Default Image By Me (If No Link Provided)
+exports.Support = `https://discord.gg/YDQFtJarhh`; //Support Server Link - Never Gonna Give You Up (If No Link Provided)
+//Make Sure To On Intents!
